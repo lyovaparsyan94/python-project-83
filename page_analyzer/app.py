d08b2e6eda9e09515b0c4ecd876ea3c9b24d887d
Parsyan Lyova <l.parsyan@blask.com>
2025-03-19 13:21:30 +0400

    feat: add base templates and update Flask app
    - Added ase.html, ooter.html, header.html, and index.html to page_analyzer/templates for structuring the UI.
    - Updated pp.py to render index.html as the main page using Flask's ender_template.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,4 +12,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to the "Page Analyzer"!'
+    return render_template(
+        'index.html',
+    )
